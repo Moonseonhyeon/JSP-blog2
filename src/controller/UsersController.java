17815aebc6b70b3639b1e9f2ae1c01b2ca90c5ce
Linda <msh1730@hanmail.net>
2020-06-07 13:52:50 +0900

    Controller 기본 세팅 완료 - 단점: Controller가 지저분 해짐

---
+++ src/controller/UsersController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//http://localhost:8000/blog/user
+@WebServlet("/user")
+public class UsersController extends HttpServlet {
+	private static final String TAG = "UserController : ";
+	private static final long serialVersionUID = 1L;
+ 
+    public UsersController() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//http://localhost:8000/blog2/user?cmd=join
+		String cmd = request.getParameter("cmd");
+		System.out.println(TAG+"doProcess : "+cmd);
+		if (cmd.equals("join")) {
+			//회원가입 페이지로 이동
+		} else if(cmd.equals("joinProc")) {
+			//회원가입 진행 한후 -> Index.jsp로 이동
+		} else if(cmd.equals("update")) {
+			//회원수정 페이지로 이동 (세션에 User 오브젝트를 가지고 있을 예정)
+		} else if(cmd.equals("updateProc")) {
+			//회원수정을 진행한후 -> index.jsp로 이동
+		} else if(cmd.equals("delete")) {
+			//회원삭제를 진행한 후 -> 로그아웃을 하고 -> index.jsp로 이동
+		} else if(cmd.equals("login")) {
+			//회원 로그인 페이지로 이동
+		} else if(cmd.equals("loginProc")) {
+			//회원 로그인을 수행 한 후-> 세션 등록을 하고 -> index.jsp로 이동
+		} 
+	}
+
+}
